e6f8029e9587f27805bfbadc8b18f2fa008ddbed
Andrés Nuñez <andresanunez98@gmail.com>
2021-09-06 12:28:07 -0400

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,9 @@ Medication.destroy_all
 
 
     Medication.create(
-        name: "Venlafaxine", 
+        name: "MEDICATION I", 
         directions: "take 1 tablet by mouth twice a day",
-        brand_name: "Effexor",
+        brand_name: "sample brand name",
         medication_dose: "75 mg",
         medication_description: "peach round-shaped tablet imprinted with TV in the front and 4 K on the back",
         last_taken: "2000-01-01 00:00:25",
@@ -34,9 +34,9 @@ Medication.destroy_all
     )
 
     Medication.create(
-        name: "Alprazolam", 
+        name: "MEDICATION II", 
         directions: "take 1 tablet by mouth once a day",
-        brand_name: "Xanax",
+        brand_name: "sample brand name",
         medication_dose: "0.5 mg",
         medication_description: "peach oval-shaped tablet imprinted with 1 9 in the front and a Y on the back",
         last_taken: "2000-01-01 00:00:25",
